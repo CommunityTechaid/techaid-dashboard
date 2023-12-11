21fd6348c2351cc90cef2ce205fd2d33b45a655c
Akhil Muraleedharan <akhil.hardys@gmail.com>
2023-12-11 15:47:30 +0000

    Added a custom notes component (PoC version) that is responsible for displaying the notes.  It's super ugly and impractical atm but it is there and it works. Need to update the CSS to make the edit and delete buttons small icons and add the email of the volunteer who added the information along with the date. Also have to make the whole thing a scrollable comment box style widget

---
+++ src/app/views/corewidgets/components/kit-info/custom-notes.ts
@@ -0,0 +1,94 @@
+import { Component } from '@angular/core';
+import { FieldType } from '@ngx-formly/core';
+import { ToastrService } from 'ngx-toastr';
+import gql from 'graphql-tag';
+import { Apollo } from 'apollo-angular';
+
+
+const UPDATE_NOTE = gql`
+mutation updateNote($data: UpdateNoteInput!) {
+  updateNote(data: $data){
+      content
+      volunteer
+      createdAt
+      updatedAt
+      id
+  }
+}
+`;
+
+const DELETE_NOTE = gql`
+mutation deleteNote($id: ID!) {
+  deleteNote(id: $id)
+}
+`;
+
+@Component({
+    selector: 'formly-field-notes',
+    template: `
+  <div class="comment-box">
+  <div *ngFor="let note of to.notes" class="note-item">
+    <span>{{ note.content }}</span>
+    <div class="actions">
+      <button (click)="updateNote({content: 'Updated note', id: note.id})">Edit</button>
+      <button (click)="deleteNote(note.id)">Delete</button>
+    </div>
+  </div>
+</div>
+ `,
+})
+export class FormlyCustomNote extends FieldType {
+
+
+    constructor(
+        private toastr: ToastrService,
+        private apollo: Apollo
+    ) {
+        super();
+    }
+
+    //todo edit to remove reloading and instead do a subscribe and refresh thingy to eliminate unecessary reloading
+    updateNote(data: any) {
+
+        this.apollo.mutate({
+            mutation: UPDATE_NOTE,
+            variables: {
+                data: data
+            }
+        }).subscribe(res => {
+            this.toastr.info(`
+          <small>Successfully updated note</small>
+          `, 'Updated Note', {
+                enableHtml: true
+            });
+            location.reload();
+        }, err => {
+            this.toastr.error(`
+          <small>${err.message}</small>
+          `, 'Update Error', {
+                enableHtml: true
+            });
+        });
+    }
+
+    //todo edit to remove reloading and instead do a subscribe and refresh thingy to eliminate unecessary reloading
+    deleteNote(id: any) {
+        this.apollo.mutate<any>({
+            mutation: DELETE_NOTE,
+            variables: { id: id }
+        }).subscribe(res => {
+            this.toastr.info(`
+          <small>Successfully deleted note</small>
+          `, 'Deleted Note', {
+                enableHtml: true
+            });
+            location.reload();
+        }, err => {
+            this.toastr.error(`
+          <small>${err.message}</small>
+          `, 'Error Deleting Note', {
+                enableHtml: true
+            });
+        });
+    }
+}
