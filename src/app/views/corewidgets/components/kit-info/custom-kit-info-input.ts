df52e1dd106eae5db5d92bce2cb36153cbc1cf88
Akhil Muraleedharan <akhil.hardys@gmail.com>
2024-01-29 16:18:55 +0000

    Cosmetic changes to the labels

--- src/app/views/corewidgets/components/kit-info/custom-kit-info-input.ts
+++ src/app/views/corewidgets/components/kit-info/custom-kit-info-input.ts
@@ -6,11 +6,18 @@ import { FieldType, FieldTypeConfig } from '@ngx-formly/core';
   styleUrls: ['kit-info.scss'],
   template: `
   <label *ngIf="to.label">{{to.label}}</label>
-    <div class="kit-info-input d-flex w-100 align-items-end justify-content-center">
+    <div class="kit-info-input d-flex w-100 align-items-end justify-content-between">
+    <div>
       <input [hidden]="!editable" type='input' [formControl]="formControl" [formlyAttributes]="field" [(ngModel)]="infoValue">      
-      <span [hidden]="editable" class="pr-1"  [innerText]="infoValue"></span>
-      <span class="pr-1" *ngIf="to.descriptor">{{to.descriptor}}</span>
+      <span *ngIf="infoValue != undefined" [hidden]="editable" class="pr-1"  [innerText]="infoValue"></span>
+      <span *ngIf="infoValue == undefined" [hidden]="editable" class="pr-1 pl-1">X</span>
+      <span class="pr-1" *ngIf="to.descriptor">{{to.descriptor }}</span>
+    </div>
+    <div>
       <i (click)="test()" class="fas fa-edit fa-xs"></i>
+    </div>
+      
+      
     </div>
   `,
 })
