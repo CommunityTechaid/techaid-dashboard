90e2f74a6d2c84227808890632e612f7449e0656
Akhil Muraleedharan <akhil.hardys@gmail.com>
2024-01-30 15:05:58 +0000

    Minor tweaks to the UI

--- src/app/views/corewidgets/components/kit-info/custom-kit-info-input.ts
+++ src/app/views/corewidgets/components/kit-info/custom-kit-info-input.ts
@@ -30,7 +30,7 @@ Ideally, the input field should be dynamically rendered using custom selector bu
   styleUrls: ['kit-info.scss'],
   template: `
   <label *ngIf="to.label">{{to.label}}</label>
-    <div class="kit-info-input d-flex w-100 align-items-end justify-content-between">
+    <div class="kit-info-input d-flex w-100 align-items-center justify-content-between">
     <div style="font-size:smaller" class="d-flex">
 
       <select *ngIf="to.type=='select'" [hidden]="!editable" (focusout)="toggleEdit()" [formControl]="formControl" [formlyAttributes]="field" [(ngModel)]="infoValue">
@@ -42,11 +42,11 @@ Ideally, the input field should be dynamically rendered using custom selector bu
 
       <input *ngIf="to.type!='select'" [hidden]="!editable" (focusout)="toggleEdit()" [type]="to.type" [formControl]="formControl" [formlyAttributes]="field" [(ngModel)]="infoValue">      
       <span *ngIf="infoValue != undefined" [hidden]="editable" class="pr-1"  [innerText]="infoValue"></span>
-      <span *ngIf="infoValue == undefined" [hidden]="editable" class="pr-1 pl-1">X</span>
+      <span *ngIf="infoValue == undefined" [hidden]="editable" class="pr-1 pl-1">None</span>
       <span class="pr-1" *ngIf="to.descriptor">{{to.descriptor }}</span>
     </div>
     <div>
-      <i (click)="editField()" class="fas fa-edit fa-xs"></i>
+      <i (click)="editField()" class="fas fa-edit fa-xs align-self-end"></i>
     </div>
       
       
