51f4921751627945c0903bf0fec2cf41eda35cbd
Tony Anzelmo <tony.anzelmo@gmail.com>
2024-07-05 00:00:26 +0100

    Referring Org Info

--- src/app/views/corewidgets/components/referring-organisation-info/referring-organisation-info.component.ts
+++ src/app/views/corewidgets/components/referring-organisation-info/referring-organisation-info.component.ts
@@ -1,15 +1,341 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ViewChild, ViewEncapsulation } from '@angular/core';
+import { Subject, of, forkJoin, Observable, Subscription } from 'rxjs';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { ToastrService } from 'ngx-toastr';
+import gql from 'graphql-tag';
+import { Apollo } from 'apollo-angular';
+import { FormGroup } from '@angular/forms';
+import { FormlyFormOptions, FormlyFieldConfig } from '@ngx-formly/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Select } from '@ngxs/store';
+import { UserState } from '@app/state/state.module';
+import { User } from '@app/state/user/user.state';
+
+const QUERY_ENTITY = gql`
+  query findReferringOrganisation($id: Long) {
+    referringOrganisation(where: { id: { _eq: $id } }) {
+      id
+      phoneNumber
+      name
+      domain
+      address
+      website
+      createdAt
+      updatedAt
+    }
+  }
+`;
+
+const UPDATE_ENTITY = gql`
+  mutation updateReferringOrganisation($data: UpdateReferringOrganisationInput!) {
+    updateReferringOrganisation(data: $data) {
+      id
+      phoneNumber
+      name
+      domain
+      address
+      website
+      createdAt
+      updatedAt
+    }
+  }
+`;
+
+const DELETE_ENTITY = gql`
+  mutation deleteReferringOrganisation($id: ID!) {
+    deleteReferringOrganisation(id: $id)
+  }
+`;
 
 @Component({
   selector: 'app-referring-organisation-info',
   templateUrl: './referring-organisation-info.component.html',
   styleUrls: ['./referring-organisation-info.component.scss']
 })
-export class ReferringOrganisationInfoComponent implements OnInit {
+export class ReferringOrganisationInfoComponent {
 
-  constructor() { }
+  constructor(
+    private modalService: NgbModal,
+    private activatedRoute: ActivatedRoute,
+    private router: Router,
+    private toastr: ToastrService,
+    private apollo: Apollo
+  ) {}
+  sub: Subscription;
+  form: FormGroup = new FormGroup({});
+  options: FormlyFormOptions = {
+    formState: {
+      disabled: true
+    }
+  };
+  model: any = {};
+  name: string;
+  orgId: number;
+  public user: User;
+  @Select(UserState.user) user$: Observable<User>;
 
-  ngOnInit(): void {
+  fields: Array<FormlyFieldConfig> = [
+    {
+      key: 'name',
+      type: 'input',
+      className: 'col-md-12',
+      defaultValue: '',
+      templateOptions: {
+        label: 'Name',
+        placeholder: '',
+        required: true,
+      },
+      validation: {
+        show: false,
+      },
+      expressionProperties: {
+        'validation.show': 'model.showErrorState',
+        'templateOptions.disabled': 'formState.disabled',
+      },
+    },
+    {
+      fieldGroupClassName: 'row',
+      fieldGroup: [
+        {
+          key: 'website',
+          type: 'input',
+          className: 'col-md-6',
+          defaultValue: '',
+          templateOptions: {
+            label: 'Website',
+            required: false
+          },
+          validation: {
+            show: false,
+          },
+          expressionProperties: {
+            'validation.show': 'model.showErrorState',
+            'templateOptions.disabled': 'formState.disabled',
+          },
+        },
+        {
+          key: 'phoneNumber',
+          type: 'input',
+          className: 'col-md-6',
+          defaultValue: '',
+          templateOptions: {
+            label: 'Phone Number',
+            required: false
+          },
+          validation: {
+            show: false,
+          },
+          expressionProperties: {
+            'validation.show': 'model.showErrorState',
+            'templateOptions.disabled': 'formState.disabled',
+          },
+        },
+      ],
+    },
+    {
+      fieldGroupClassName: 'row',
+      fieldGroup: [
+        {
+          key: 'domain',
+          type: 'input',
+          className: 'col-md-6',
+          defaultValue: '',
+          templateOptions: {
+            label: 'Domain',
+            required: false
+          },
+          validation: {
+            show: false,
+          },
+          expressionProperties: {
+            'validation.show': 'model.showErrorState',
+            'templateOptions.disabled': 'formState.disabled',
+          },
+        },
+        {
+          key: 'address',
+          type: 'input',
+          className: 'col-md-6',
+          defaultValue: '',
+          templateOptions: {
+            label: 'Address',
+            required: false
+          },
+          validation: {
+            show: false,
+          },
+          expressionProperties: {
+            'validation.show': 'model.showErrorState',
+            'templateOptions.disabled': 'formState.disabled',
+          },
+        },
+      ],
+    },
+  ];
+
+
+  private queryRef = this.apollo.watchQuery({
+    query: QUERY_ENTITY,
+    variables: {},
+  });
+
+  modal(content) {
+    this.modalService.open(content, { centered: true });
   }
 
+  private normalizeData(data: any) {
+    if (data.subGroup) {
+      if (!Array.isArray(data.subGroup)) {
+        data.subGroup = (data.subGroup || '')
+          .toString()
+          .split(',')
+          .filter((value) => value.trim().length > 0);
+      }
+    } else {
+      data.subGroup = [];
+    }
+    data.attributes = data.attributes || {accepts: []};
+    return data;
+  }
+
+  private fetchData() {
+    if (!this.orgId) {
+      return;
+    }
+
+    this.queryRef
+      .refetch({
+        id: this.orgId,
+      })
+      .then(
+        (res) => {
+          if (res.data && res.data['referringOrganisation']) {
+            const data = res.data['referringOrganisation'];
+            this.model = this.normalizeData(data);
+            this.name = this.model['name'];
+          } else {
+            this.model = {};
+            this.name = 'Not Found!';
+          }
+        },
+        (err) => {
+          this.toastr.warning(
+            `
+          <small>${err.message}</small>
+        `,
+            'GraphQL Error',
+            {
+              enableHtml: true,
+              timeOut: 15000,
+              disableTimeOut: true,
+            }
+          );
+        }
+      );
+  }
+
+  ngOnInit() {
+    this.sub = this.activatedRoute.params.subscribe((params) => {
+      this.orgId = +params['orgId'];
+      this.fetchData();
+    });
+    this.sub.add(
+      this.user$.subscribe((user) => {
+        this.user = user;
+        this.options.formState.disabled = !(user && user.authorities && user.authorities['write:organisations']);
+      })
+    );
+  }
+
+  ngOnDestroy() {
+    if (this.sub) {
+      this.sub.unsubscribe();
+    }
+  }
+
+  updateEntity(data: any) {
+    if (!this.form.valid) {
+      this.model['showErrorState'] = true;
+      return;
+    }
+    data.id = this.orgId;
+    data.subGroup = data.subGroup || [];
+    if (data.organizing) {
+      if (data.organizing == 'yes') {
+        data.subGroup.push('Organizing');
+      }
+    }
+    data.subGroup = (data.subGroup || [])
+      .filter((v, i, a) => a.indexOf(v) === i)
+      .join(',');
+    delete data.organizing;
+    this.apollo
+      .mutate({
+        mutation: UPDATE_ENTITY,
+        variables: {
+          data,
+        },
+      })
+      .subscribe(
+        (res) => {
+          this.model = this.normalizeData(res.data['updateReferringOrganisation']);
+          this.name = this.model['name'];
+          this.toastr.info(
+            `
+      <small>Successfully updated referring organisation ${this.name}</small>
+      `,
+            'Updated Referring Organisation',
+            {
+              enableHtml: true,
+            }
+          );
+        },
+        (err) => {
+          this.toastr.error(
+            `
+      <small>${err.message}</small>
+      `,
+            'Update Error',
+            {
+              enableHtml: true,
+            }
+          );
+        }
+      );
+  }
+
+  deleteEntity() {
+    this.apollo
+      .mutate<any>({
+        mutation: DELETE_ENTITY,
+        variables: { id: this.orgId },
+      })
+      .subscribe(
+        (res) => {
+          if (res.data.deleteReferringOrganisation) {
+            this.toastr.info(
+              `
+        <small>Successfully deleted referring organisation ${this.name}</small>
+        `,
+              'Referring Organisation Deleted',
+              {
+                enableHtml: true,
+              }
+            );
+            this.router.navigate(['/dashboard/referring-organisations']);
+          }
+        },
+        (err) => {
+          this.toastr.error(
+            `
+      <small>${err.message}</small>
+      `,
+            'Error Deleting Referring Organisation',
+            {
+              enableHtml: true,
+            }
+          );
+        }
+      );
+  }
 }
