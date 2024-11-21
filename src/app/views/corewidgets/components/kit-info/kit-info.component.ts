7642279b0528b27dec9695e0a241cdabe03f6fc0
Tony Anzelmo <tony.anzelmo@gmail.com>
2024-11-21 14:40:15 +0000

    Device-specific options

--- src/app/views/corewidgets/components/kit-info/kit-info.component.ts
+++ src/app/views/corewidgets/components/kit-info/kit-info.component.ts
@@ -444,27 +444,11 @@ export class KitInfoComponent {
           className: 'col-md-4',
           fieldGroup: [
             {
-              key: 'attributes.otherType',
-              type: 'input',
-              className: '',
-              defaultValue: '',
-              templateOptions: {
-                label: 'Type of device',
-                rows: 2,
-                placeholder: '(Other device type)',
-                required: true
-              },
-              hideExpression: 'model.type != \'OTHER\'',
-              expressionProperties: {
-                'templateOptions.required': 'model.type == \'OTHER\'',
-              },
-            },
-            {
               key: 'network',
               type: 'choice',
               className: '',
               templateOptions: {
-                label: 'Is the phone currently locked to a specific network provider?',
+                label: 'Network provider?',
                 description: '',
                 items: [
                   {label: 'Phone is Unlocked', value: 'UNLOCKED' },
@@ -483,34 +467,33 @@ export class KitInfoComponent {
                 ],
                 required: true
               },
-              hideExpression: (model, state, field) => {
-                const data = field.parent.formControl.value || {};
-                return data['type'] != 'SMARTPHONE';
-              },
+              hideExpression: 'model.type != \'SMARTPHONE\''
             },
-            //Do we still need this?
-            // {
-            //   key: 'attributes.otherNetwork',
-            //   type: 'input',
-            //   className: '',
-            //   defaultValue: '',
-            //   templateOptions: {
-            //     label: 'The other network the device is locked to',
-            //     rows: 2,
-            //     placeholder: '(Other Network)',
-            //     required: true
-            //   },
-            //   hideExpression: (model, state, field) => {
-            //     const data = (field.parent.formControl.value || {}).attributes || {};
-            //     return data['network'] != 'OTHER';
-            //   },
-            //   expressionProperties: {
-            //     'templateOptions.required': (model, state, field) => {
-            //       const data = (field.parent.formControl.value || {}).attributes || {};
-            //       return data['network'] == 'OTHER';
-            //     },
-            //   },
-            // },
+            {
+              key: 'network',
+              type: 'choice',
+              className: '',
+              templateOptions: {
+                label: 'Network provider?',
+                description: '',
+                items: [
+                  {label: 'I don\'t know', value: 'UNKNOWN' },
+                  {label: 'EE', value: 'EE' },
+                  {label: 'O2', value: 'O2' },
+                  {label: 'Three', value: 'Three' },
+                  {label: 'Vodafone', value: 'Vodafone' },
+                  {label: 'GiffGaff', value: 'GiffGaff' },
+                  {label: 'Sky Mobile', value: 'SkyMobile' },
+                  {label: 'Tesco Mobile', value: 'TescoMobile' },
+                  {label: 'BT Mobile', value: 'BTMobile' },
+                  {label: 'Virgin Mobile', value: 'VirginMobile' },
+                  {label: 'Talk Talk', value: 'TalkTalk' },
+                  {label: 'Other', value: 'OTHER' }
+                ],
+                required: true
+              },
+              hideExpression: 'model.type != \'COMMSDEVICE\''
+            },
             {
                 key: 'installedOSName',
                 type: 'choice',
@@ -522,7 +505,11 @@ export class KitInfoComponent {
                   items: []
                 },
                 defaultValue: [],
-                hideExpression: 'model.type == \'OTHER\'',
+                hideExpression: (model, state, field) => {
+                  const data = field.parent.formControl.value || {};
+                  const unSupportedDevices = ['OTHER','COMMSDEVICE'];
+                  return data['type'] in unSupportedDevices;
+                },
                 expressionProperties: {
                   'templateOptions.items': (model, state, field) => {
                     const desktopOS = [
@@ -561,7 +548,11 @@ export class KitInfoComponent {
                 label: 'Locked to user?',
                 required: false
               },
-              hideExpression: 'model.type != \'SMARTPHONE\' && model.type != \'TABLET\'',
+              hideExpression: (model, state, field) => {
+                const data = field.parent.formControl.value || {};
+                const supportedDevices = ['SMARTPHONE','TABLET'];
+                return !(data['type'] in supportedDevices);
+              },
               validation: {
                 show: false
               }
@@ -577,9 +568,8 @@ export class KitInfoComponent {
               },
               hideExpression: (model, state, field) => {
                 const data = field.parent.formControl.value || {};
-                return data['type'] != 'DESKTOP' &&
-                  data['type'] != 'LAPTOP' &&
-                  data['type'] != 'ALLINONE';
+                const supportedDevices = ['DESKTOP','LAPTOP','ALLINONE'];
+                return !(data['type'] in supportedDevices);
               },
               validation: {
                 show: false
@@ -594,7 +584,11 @@ export class KitInfoComponent {
                 label: 'OS Installation failed?',
                 required: false
               },
-              hideExpression: 'model.type == \'OTHER\'',
+              hideExpression: (model, state, field) => {
+                const data = field.parent.formControl.value || {};
+                const unSupportedDevices = ['OTHER','COMMSDEVICE'];
+                return data['type'] in unSupportedDevices;
+              },
               validation: {
                 show: false
               }
@@ -630,18 +624,9 @@ export class KitInfoComponent {
         {
             fieldGroupClassName: 'd-flex flex-column justify-content-between',
             className: 'col-md-4',
-            fieldGroup: [
-              /* {
-              type: 'textarea',
-              className: '',
-              defaultValue: '',
-              templateOptions: {
-                label: 'Add new note about device',
-                rows: 4,
-                required: false,
-                placeholder: 'Enter text and your initials. The current date and time will be automatically added to the comment'
-              }
-            }, */
+          fieldGroupClassName: 'd-flex flex-column justify-content-between',
+          className: 'col-md-4',
+          fieldGroup: [
             this.newNoteField,
             this.notesField,
             {
@@ -658,228 +643,93 @@ export class KitInfoComponent {
                 ],
                 required: true,
               }
-            },
-            // {
-            //   template: `
-            //   <div class="alert alert-warning shadow" role="alert">
-            //     Please ensure the donor has been updated with the reasons why
-            //     the donation has been <span class="badge badge-danger">DECLINED<span>
-            //   </div>
-            //   `,
-            //   hideExpression: 'model.status != \'DECLINED\''
-            // },
+            }
           ]
         }
-        /* ,{
-          fieldGroupClassName: 'd-none flex-column justify-content-between',
-          className: 'col-md-4',
-          fieldGroup: [
-            {
-              key: 'location',
-              type: 'place',
-              className: 'col-md-12',
-              defaultValue: '',
-              templateOptions: {
-                label: 'Location of device',
-                placeholder: '',
-                postCode: false,
-                required: true
-              }
-            },
-            this.techniciansField,
-            this.organisersField,
-            this.logisticsField,
-          ]
-        } */
       ]
-    },
+    }
+  ];
+
+// {
+//   key: 'attributes.otherType',
+//   type: 'input',
+//   className: '',
+//   defaultValue: '',
+//   templateOptions: {
+//     label: 'Type of device',
+//     rows: 2,
+//     placeholder: '(Other device type)',
+//     required: true
+//   },
+//   hideExpression: 'model.type != \'OTHER\'',
+//   expressionProperties: {
+//     'templateOptions.required': 'model.type == \'OTHER\'',
+//   },
+// },
+
+  /* {
+  type: 'textarea',
+  className: '',
+  defaultValue: '',
+  templateOptions: {
+    label: 'Add new note about device',
+    rows: 4,
+    required: false,
+    placeholder: 'Enter text and your initials. The current date and time will be automatically added to the comment'
+  }
+}, */
 
 
-    // {
-    //   template: `
-    //   <div class="row">
-    //     <div class="col-md-12">
-    //       <div class="border-bottom-info card mb-3 p-3">
-    //         <strong><p>About your device</p></strong>
-    //         <p>
-    //           In order to understand what condition your device is in - and how easy it will be for us
-    //           to get it ready to deliver - please answer as many of the following questions as you can.
-    //         </p>
-    //       </div>
-    //     </div>
-    //   </div>
-    //   `
-    // },
+//Do we still need this?
+// {
+//   key: 'attributes.otherNetwork',
+//   type: 'input',
+//   className: '',
+//   defaultValue: '',
+//   templateOptions: {
+//     label: 'The other network the device is locked to',
+//     rows: 2,
+//     placeholder: '(Other Network)',
+//     required: true
+//   },
+//   hideExpression: (model, state, field) => {
+//     const data = (field.parent.formControl.value || {}).attributes || {};
+//     return data['network'] != 'OTHER';
+//   },
+//   expressionProperties: {
+//     'templateOptions.required': (model, state, field) => {
+//       const data = (field.parent.formControl.value || {}).attributes || {};
+//       return data['network'] == 'OTHER';
+//     },
+//   },
+// },
 
-  ]
 
+// {
+//   template: `
+//   <div class="alert alert-warning shadow" role="alert">
+//     Please ensure the donor has been updated with the reasons why
+//     the donation has been <span class="badge badge-danger">DECLINED<span>
+//   </div>
+//   `,
+//   hideExpression: 'model.status != \'DECLINED\''
+// },
 
-        //Can mimic this for device type OS
-        // {
-        //   className: 'col-md-6',
-        //   fieldGroup: [
-            // {
-            //   key: 'attributes.status',
-            //   type: 'multicheckbox',
-            //   className: '',
-            //   templateOptions: {
-            //     type: 'array',
-            //     options: [],
-            //     description: 'Please select all options that apply'
-            //   },
-            //   defaultValue: [],
-            //   expressionProperties: {
-            //     'templateOptions.options': (model, state, field) => {
-            //       const props = {
-            //         'LAPTOP': [
-            //           {label: 'I have the charger / power cable for the Laptop', value: 'CHARGER'},
-            //           {label: 'I don\'t have the charger / power cable for the Laptop', value: 'NO_CHARGER'},
-            //           {label: 'I have a password set for the Laptop', value: 'PASSWORD_PROTECTED'},
-            //           {label: 'I don\'t have a password set for the Laptop', value: 'NO_PASSWORD'}
-            //         ],
-            //         'TABLET': [
-            //           {label: 'I have the charger for the Tablet', value: 'CHARGER'},
-            //           {label: 'I don\'t have the charger / power cable for the Tablet', value: 'NO_CHARGER'},
-            //           {label: 'Have you factory reset the Tablet?', value: 'FACTORY_RESET'}
-            //         ],
-            //         'SMARTPHONE': [
-            //           {label: 'I have the charger for the Phone', value: 'CHARGER'},
-            //           {label: 'I don\'t have the charger / power cable for the Phone', value: 'NO_CHARGER'},
-            //           {label: 'Have you factory reset the Phone?', value: 'FACTORY_RESET'}
-            //         ],
-            //         'ALLINONE': [
-            //           {label: 'I have the charger for the Computer', value: 'CHARGER'},
-            //           {label: 'I don\'t have the charger / power cable for the Computer', value: 'NO_CHARGER'},
-            //           {label: 'Do you have a mouse for the Computer?', value: 'HAS_MOUSE'},
-            //           {label: 'Do you have a keyboard for the Computer', value: 'HAS_KEYBOARD'},
-            //           {label: 'I have a password set for the Computer', value: 'PASSWORD_PROTECTED'},
-            //           {label: 'I don\'t have a password set for the Computer', value: 'NO_PASSWORD'}
-            //         ],
-            //         'DESKTOP': [
-            //           {label: 'I have the power cable for the Computer', value: 'CHARGER'},
-            //           {label: 'I don\'t have the power cable for the Computer', value: 'NO_CHARGER'},
-            //           {label: 'Do you have a mouse for the Computer?', value: 'HAS_MOUSE'},
-            //           {label: 'Do you have a keyboard for the Computer', value: 'HAS_KEYBOARD'},
-            //           {label: 'I have a password set for the Computer', value: 'PASSWORD_PROTECTED'},
-            //           {label: 'I don\'t have a password set for the Computer', value: 'NO_PASSWORD'}
-            //         ],
-            //         'OTHER': [
-            //           {label: 'I have the charger or power cable for the device', value: 'CHARGER'},
-            //           {label: 'I don\'t have the charger / power cable for the device', value: 'NO_CHARGER'},
-            //         ],
-            //         'COMMSDEVICE': [
-            //           {label: 'Mobile SIM card', value: 'MOBILE_SIM'},
-            //           {label: 'Data SIM card', value: 'DATA_SIM'},
-            //           {label: 'Dongle with SIM', value: 'DONGLE_SIM'},
-            //           {label: 'MiFi with SIM', value: 'MIFI_SIM'}
-            //         ],
-            //       };
-            //       let values = props[model['type']] || props['OTHER'];
-            //       const delta = {
-            //         'CHARGER': ['NO_CHARGER'],
-            //         'NO_CHARGER': ['CHARGER'],
-            //         'PASSWORD_PROTECTED': ['NO_PASSWORD'],
-            //         'NO_PASSWORD': ['PASSWORD_PROTECTED'],
-            //         'MOBILE_SIM': ['DATA_SIM', 'DONGLE_SIM', 'MIFI_SIM'],
-            //         'DATA_SIM': ['DONGLE_SIM', 'MIFI_SIM', 'MOBILE_SIM'],
-            //         'DONGLE_SIM': ['MIFI_SIM', 'MOBILE_SIM', 'DATA_SIM'],
-            //         'MIFI_SIM': ['MOBILE_SIM', 'DATA_SIM', 'DONGLE_SIM']
-            //       };
-            //       (field.formControl.value || []).forEach(val => {
-            //         if (delta[val]) {
-            //           values = values.filter(v => !delta[val].includes(v.value));
-            //         }
-            //       });
-            //       return values;
-            //     },
-            //   },
-            // },
-            // {
-            //   key: 'attributes.credentials',
-            //   type: 'input',
-            //   className: '',
-            //   defaultValue: '',
-            //   templateOptions: {
-            //     label: 'Device Password',
-            //     description: 'If your device requires a password or a PIN to sign in, please provide it here',
-            //     rows: 2,
-            //     placeholder: 'Password',
-            //     required: false
-            //   },
-            //   hideExpression: (model, state, field) => {
-            //     const data = field.parent.formControl.value || {};
-            //     const status = data.attributes.status || [];
-            //     if (status && status.length) {
-            //       return status.indexOf('PASSWORD_PROTECTED') === -1;
-            //     }
-            //     return true;
-            //   }
-            // },
-        //   ]
-        // },
-    //     {
-    //       key: 'age',
-    //       type: 'radio',
-    //       className: 'col-md-6',
-    //       templateOptions: {
-    //         label: 'Roughly how old is your device?',
-    //         options: [
-    //           {label: 'Less than a year', value: 1},
-    //           {label: '1 - 2 years', value: 2},
-    //           {label: '3 - 4 years', value: 4 },
-    //           {label: '5 - 6 years', value: 5},
-    //           {label: 'More than 6 years old', value: 6 },
-    //           {label: 'I don\'t know!', value: 0 }
-    //         ],
-    //         required: true
-    //       }
-    //     }
-    //   ]
-    // },
-
-    // {
-    //   key: 'attributes.state',
-    //   type: 'input',
-    //   className: 'col-md-12',
-    //   defaultValue: '',
-    //   templateOptions: {
-    //     label: 'What technical state is the device in? For example, does it turn on OK? Are there keys missing? Is the screen cracked?',
-    //     rows: 2,
-    //     placeholder: '',
-    //     required: false
-    //   }
-    // },
-    // {
-    //   template: `
-    //   <div class="row">
-    //     <div class="col-md-12">
-    //       <div class="border-bottom-warning card mb-3 p-3">
-    //         <p>
-    //           In order to protect your data, Community TechAid will delete any personal information
-    //           submitted via this form as soon as it has been used for collecting and delivering your device.
-    //           Alternatively, if we don't collect your device, we will delete your information immediately.
-    //           We promise to process your data in accordance with data protection legislation, and will not
-    //           share your details with any third parties. You have the right to ask for your information to be
-    //           deleted from our records - please contact contact@communitytechaid.org.uk for more information.
-    //         </p>
-    //       </div>
-    //     </div>
-    //   </div>
-    //   `
-    // },
-    // {
-    //   key: 'attributes.consent',
-    //   type: 'radio',
-    //   className: 'col-md-12',
-    //   templateOptions: {
-    //     label: '',
-    //     options: [
-    //       {label: 'I consent to my data being processed by Community TechAid', value: 'yes' },
-    //       {label: 'I do not consent to my data being processed by Community TechAid', value: 'no' },
-    //     ],
-    //     required: true
-    //   }
-    // }
+// {
+//   template: `
+//   <div class="row">
+//     <div class="col-md-12">
+//       <div class="border-bottom-info card mb-3 p-3">
+//         <strong><p>About your device</p></strong>
+//         <p>
+//           In order to understand what condition your device is in - and how easy it will be for us
+//           to get it ready to deliver - please answer as many of the following questions as you can.
+//         </p>
+//       </div>
+//     </div>
+//   </div>
+//   `
+// },
 
 
   private queryRef = this.apollo
